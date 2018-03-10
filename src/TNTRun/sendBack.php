a08ea7201478781795c4d46f9583e3b28bd1adc9
BeckenPlayZMCPE <beckencraft123@gmail.com>
2018-03-10 18:30:23 +0500

    Add files via upload

---
+++ src/TNTRun/sendBack.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace TNTRun;
+
+use pocketmine\tile\Sign;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\level\Level;
+use pocketmine\Player;
+use pocketmine\level\Position;
+
+class sendBack extends PluginTask {
+
+    public $prefix;
+
+    public function __construct($plugin, Player $player) {
+        $this->plugin = $plugin;
+        $this->player = $player;
+        parent::__construct($plugin);
+        $this->prefix = $this->plugin->getConfig()->get("prefix");
+    }
+
+    public function onRun($tick) {
+        $level = $this->plugin->getServer()->getDefaultLevel();
+        $spawn = $level->getSafeSpawn();
+        $this->player->teleport(new Position($spawn->getX(), $spawn->getY(), $spawn->getZ(), $level), 0, 0);
+    }
+
+}
---
+++ src/TNTRun/sendPopupAgain.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TNTRun;
+
+use pocketmine\tile\Sign;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Player;
+use pocketmine\level\Level;
+
+class sendPopupAgain extends PluginTask {
+
+    public $prefix;
+
+    public function __construct($plugin, $text, Player $player) {
+        $this->plugin = $plugin;
+        $this->text = $text;
+        $this->player = $player;
+        parent::__construct($plugin, $text, $player);
+        $this->prefix = $this->plugin->getConfig()->get("prefix");
+    }
+
+    public function onRun($tick) {
+        $this->player->sendPopup($this->text);
+    }
+
+}
